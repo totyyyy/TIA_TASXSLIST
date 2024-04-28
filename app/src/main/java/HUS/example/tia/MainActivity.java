df48368925cbc6bebbce1ad821e63d95d6779afc
Nardeen230505 <nardeeno2005@gmail.com>
2024-04-28 15:02:38 +0300

    يما

--- app/src/main/java/HUS/example/tia/MainActivity.java
+++ app/src/main/java/HUS/example/tia/MainActivity.java
@@ -1,8 +1,12 @@
 package HUS.example.tia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        getMenuInflater().inflate(R.menu.main_menu,menu);
+        return true;
+    }
+
+    @Override//معالجة حدث اختيار عنصر من القائمة
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.itemSettings)
+        {
+            Toast.makeText(this, "settings", Toast.LENGTH_SHORT).show();
+        }
+        if(item.getItemId()==R.id.itemSingOut)
+        {
+            Toast.makeText(this, "signout", Toast.LENGTH_SHORT).show();
+        }
+        if(item.getItemId()==R.id.itemHistory)
+        {
+            Toast.makeText(this, "history", Toast.LENGTH_SHORT).show();
+        }
+        return true;
     }
 }
